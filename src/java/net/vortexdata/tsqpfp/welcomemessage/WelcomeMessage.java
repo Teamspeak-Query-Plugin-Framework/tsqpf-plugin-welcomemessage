dd9eae7604a2656a4d0a4371fe0a7ddc73b6b6ea
TAXSET <sandro.kierner@gmail.com>
2019-12-27 15:06:16 +0100

    Added more placeholders

---
+++ src/java/net/vortexdata/tsqpfp/welcomemessage/WelcomeMessage.java
@@ -0,0 +1,34 @@
+package net.vortexdata.tsqpfp.welcomemessage;
+
+import com.github.theholywaffle.teamspeak3.api.event.*;
+import net.vortexdata.tsqpf.plugins.*;
+import net.vortexdata.tsqpfp.welcomemessage.*;
+
+public class WelcomeMessage extends TeamspeakPlugin {
+
+    private MessageProcessor messageProcessor;
+
+    @Override
+    public void onEnable() {
+        getConfig().setDefault("messageWelcome", "Welcome %clientNickname% to this Teamspeak server! There are currently %onlineOtherUsers% other users online. ");
+        getConfig().setDefault("staffGroups", "1");
+        getConfig().saveAll();
+
+        messageProcessor = new MessageProcessor(getAPI(), getConfig());
+    }
+
+    @Override
+    public void onDisable() {
+        messageProcessor = null;
+    }
+
+    @Override
+    public void onClientJoin(ClientJoinEvent clientJoinEvent) {
+        try {
+            getAPI().sendPrivateMessage(clientJoinEvent.getClientId(), messageProcessor.process(getConfig().readValue("messageWelcome")));
+        } catch (Exception e) {
+            getLogger().printDebug("Failed to send private message to " + clientJoinEvent.getInvokerName() + ". This user probably disconnected before the private message could be sent.");
+        }
+
+    }
+}
